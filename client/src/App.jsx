b9a5e9800ff4e545461c10f90b5f2082c3f9c029
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-20 09:06:21 +0530

    Basic Project Setup For Client And Server

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App text-3xl font-bold underline">
+      Welcome To Code Playground!
+    </div>
+  )
+}
+
+export default App
